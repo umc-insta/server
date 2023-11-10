9c5968688a9c65b9699530d12692d989f689e8bb
sangjun <sangjun1389@ajou.ac.kr>
2023-11-11 04:24:05 +0900

    FileUploadController upload,delete 구현

--- src/main/java/com/umc/post/controller/FileUploadController.java
+++ src/main/java/com/umc/post/controller/FileUploadController.java
@@ -1,2 +1,31 @@
-package com.umc.post.controller;public class FileUploadController {
+package com.umc.post.controller;
+
+import com.umc.post.service.FileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/file")
+public class FileUploadController {
+
+    @Autowired
+    private final FileUploadService fileUploadService;
+
+    public FileUploadController(FileUploadService fileUploadService) {
+        this.fileUploadService = fileUploadService;
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
+        return fileUploadService.saveFile(file);
+    }
+
+    @PostMapping("/delete")
+    public void deleteFile(@RequestParam("file") String filename) {
+        fileUploadService.deleteFile(filename);
+    }
 }
